935646f814af44bd978331cdcad6617ef5e853e3
Jesper Kristensen <cableman@linuxdev.dk>
2023-03-06 14:11:53 +0100

    KLIMA-15: Added entities

---
+++ src/Entity/Screen.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ScreenRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScreenRepository::class)]
+#[ApiResource]
+class Screen
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?int $gridColumns = null;
+
+    #[ORM\Column]
+    private ?int $gridRows = null;
+
+    #[ORM\Column]
+    private array $variant = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getGridColumns(): ?int
+    {
+        return $this->gridColumns;
+    }
+
+    public function setGridColumns(int $gridColumns): self
+    {
+        $this->gridColumns = $gridColumns;
+
+        return $this;
+    }
+
+    public function getGridRows(): ?int
+    {
+        return $this->gridRows;
+    }
+
+    public function setGridRows(int $gridRows): self
+    {
+        $this->gridRows = $gridRows;
+
+        return $this;
+    }
+
+    public function getVariant(): array
+    {
+        return $this->variant;
+    }
+
+    public function setVariant(array $variant): self
+    {
+        $this->variant = $variant;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Tile.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TileRepository::class)]
+#[ApiResource]
+class Tile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $remoteUrl = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $mail = null;
+
+    #[ORM\Column(nullable: true)]
+    private array $extra = [];
+
+    #[ORM\Column]
+    private ?bool $accepted = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getRemoteUrl(): ?string
+    {
+        return $this->remoteUrl;
+    }
+
+    public function setRemoteUrl(string $remoteUrl): self
+    {
+        $this->remoteUrl = $remoteUrl;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getExtra(): array
+    {
+        return $this->extra;
+    }
+
+    public function setExtra(?array $extra): self
+    {
+        $this->extra = $extra;
+
+        return $this;
+    }
+
+    public function isAccepted(): ?bool
+    {
+        return $this->accepted;
+    }
+
+    public function setAccepted(bool $accepted): self
+    {
+        $this->accepted = $accepted;
+
+        return $this;
+    }
+}
