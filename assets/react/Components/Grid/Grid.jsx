9853adfc7ea79f023e016f79a053c5bfa9debac9
Jesper Pedersen <pejes@aarhus.dk>
2023-03-13 14:36:20 +0100

    KLIMA-36: move grid to its own component

---
+++ assets/react/Components/Grid/Grid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from 'styled-components'
+
+const Grid = (props) => {
+  return (
+    <Wrapper {...props}>
+      {props.children}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: repeat(var(--grid-rows), 1fr);
+  grid-template-columns: repeat(var(--grid-columns), 1fr);
+  gap: 0;
+`;
+
+export default Grid;
