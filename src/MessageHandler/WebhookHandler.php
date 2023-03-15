816c9a63ba2da22bfd7731dbcd7d40a9a3af2fc0
Jesper Kristensen <cableman@linuxdev.dk>
2023-03-15 14:50:19 +0100

    KILMA-15: Fixed webhook tags

--- src/MessageHandler/WebhookHandler.php
+++ src/MessageHandler/WebhookHandler.php
@@ -2,8 +2,10 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\Tags;
 use App\Entity\Tile;
 use App\Entity\Webhook;
+use App\Repository\TagsRepository;
 use App\Service\WebhookService;
 use Doctrine\ORM\EntityManagerInterface;
 use ItkDev\MetricsBundle\Service\MetricsService;
@@ -14,6 +16,7 @@ final class WebhookHandler
 {
     public function __construct(
         private readonly EntityManagerInterface $em,
+        private readonly TagsRepository $tagsRepository,
         private readonly MetricsService $metricsService,
         private readonly WebhookService $webhookService,
     ) {
@@ -35,7 +38,16 @@ final class WebhookHandler
             ->setExtra($data['extra']);
 
         foreach ($data['tags'] as $tag) {
-            $tile->addTag($tag);
+            $entity = $this->tagsRepository->findOneBy(['tag' => $tag]);
+            if (is_null($entity)) {
+                $entity = new Tags();
+                $entity->setTag($tag);
+                $this->em->persist($entity);
+
+                // Need to flush data to ensure the findOneBy above now will find the new tag in next loop.
+                $this->em->flush();
+            }
+            $tile->addTag($entity);
         }
 
         $this->em->persist($tile);
