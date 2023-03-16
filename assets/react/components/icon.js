f4a7363529730e202276cd3b731fda4530a6de94
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-03-16 13:20:11 +0100

    KLIMA-11: Changed folder organization

---
+++ assets/react/components/icon.js
@@ -0,0 +1,40 @@
+import React from "react";
+import one from '../assets/icons/single-rgb-2016-01.svg';
+import two from '../assets/icons/single-rgb-2016-02.svg';
+import tree from '../assets/icons/single-rgb-2016-03.svg';
+import four from '../assets/icons/single-rgb-2016-04.svg';
+import five from '../assets/icons/single-rgb-2016-05.svg';
+import six from '../assets/icons/single-rgb-2016-06.svg';
+import seven from '../assets/icons/single-rgb-2016-07.svg';
+import eight from '../assets/icons/single-rgb-2016-08.svg';
+import nine from '../assets/icons/single-rgb-2016-09.svg';
+import ten from '../assets/icons/single-rgb-2016-10.svg';
+import eleven from '../assets/icons/single-rgb-2016-11.svg';
+import twelve from '../assets/icons/single-rgb-2016-12.svg';
+import thirteen from '../assets/icons/single-rgb-2016-13.svg';
+import fourteen from '../assets/icons/single-rgb-2016-14.svg';
+import fifteen from '../assets/icons/single-rgb-2016-15.svg';
+import sixteen from '../assets/icons/single-rgb-2016-16.svg';
+import verdensmaal from '../assets/icons/verdensmaal-color-vertical.svg';
+
+const Icon = {
+  one,
+  two,
+  tree,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  eleven,
+  twelve,
+  thirteen,
+  fourteen,
+  fifteen,
+  sixteen,
+  verdensmaal,
+}
+
+export default Icon;
